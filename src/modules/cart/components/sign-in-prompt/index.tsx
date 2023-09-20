7c89a50b5a90a4022279e2a9e12e63c2c806445b
Makoorr <azizbenrejeb@gmail.com>
2023-09-20 23:30:20 +0100

    changed language for cart components

--- src/modules/cart/components/sign-in-prompt/index.tsx
+++ src/modules/cart/components/sign-in-prompt/index.tsx
@@ -5,14 +5,14 @@ const SignInPrompt = () => {
   return (
     <div className="bg-white flex items-start justify-between">
       <div>
-        <h2 className="text-xl-semi">Already have an account?</h2>
+        <h2 className="text-xl-semi">Vous avez déjà un compte?</h2>
         <p className="text-base-regular text-gray-700 mt-2">
-          Sign in for a better experience.
+          Se connecter pour une meilleure expérience.
         </p>
       </div>
       <div>
         <Link href="/account/login">
-          <Button variant="secondary">Sign in</Button>
+          <Button variant="secondary">Se connecter</Button>
         </Link>
       </div>
     </div>
