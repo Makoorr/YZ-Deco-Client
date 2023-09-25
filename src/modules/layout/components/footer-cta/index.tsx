aa0a1bef8cdd29dbe7e1dccc26998b506fbdaa31
Makoorr <azizbenrejeb@gmail.com>
2023-09-25 17:01:42 +0100

    fixed footer cta

--- src/modules/layout/components/footer-cta/index.tsx
+++ src/modules/layout/components/footer-cta/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const FooterCTA = () => {
   return (
     <div className="bg-amber-800 w-full" style={{ height: "60vh" }}>
-      <div className="content-container flex flex-col-reverse gap-y-8 small:flex-row small:items-center justify-between py-16 relative">
+      <div className="content-container flex flex-col-reverse gap-y-8 small:flex-row small:items-center justify-between py-16 relative" style={{ height: "100%" }}>
         <div>
           <h3 className="text-2xl-semi">Achetez les derniers styles</h3>
           <div className="mt-6">
@@ -12,7 +12,7 @@ const FooterCTA = () => {
           </div>
         </div>
 
-        <div className="relative w-full aspect-square small:w-[35%] small:aspect-[28/36]">
+        <div className="relative w-full aspect-square small:w-[35%] small:aspect-[28/36]" style={{ height: "100%" }}>
           <Image
             src="/cadeaux.jpg"
             alt=""
