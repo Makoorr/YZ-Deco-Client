3b1ce1eb34d8ba6d007876fc5def21eb3384285d
Makoorr <azizbenrejeb@gmail.com>
2023-09-30 16:52:00 +0100

    added local img upload

--- src/modules/home/components/text-description/index.tsx
+++ src/modules/home/components/text-description/index.tsx
@@ -5,8 +5,8 @@ const TextDescription = () => {
     return (
         <Description
             content = "Notre gamme de cadeaux et d&apos;accessoires s&apos;inspire de l&apos;élégance intemporelle de la mode et du style, offrant une sélection soigneusement conçue pour sublimer votre style personnel et créer des moments mémorables."
-            backgroundImage = "http://localhost:9000/uploads/description.jpg"
-        />
+            backgroundImage = "/description.jpg"
+            />
     )
 }
 export default TextDescription;
